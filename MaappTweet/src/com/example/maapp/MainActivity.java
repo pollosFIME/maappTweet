e5f90998194b29487515188b12545ee7293cb09b
hozkarr <oscar.luis.garcia94@gmail.com>
2014-02-18 18:34:30 -0600

    aqui esta ;)

--- MaappTweet/src/com/example/maapp/MainActivity.java
+++ MaappTweet/src/com/example/maapp/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.maapp;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
@@ -8,27 +15,30 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-
-public class MainActivity extends FragmentActivity implements OnMapClickListener {
+public class MainActivity extends FragmentActivity implements
+		OnMapClickListener {
 
 	private GoogleMap gmap;
+	private GoogleMap map;
 	private Marker posMarker;
+	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);;
-		gmap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		setContentView(R.layout.activity_main);
+		;
+		gmap = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
 		gmap.setMyLocationEnabled(true);
 		gmap.getUiSettings().setZoomControlsEnabled(false);
 		gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(getLocation(), 8));
 		gmap.setOnMapClickListener(this);
+		map = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+		map.setMyLocationEnabled(true);
 		
+	
 	}
 
 	@Override
@@ -41,24 +51,22 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 	@Override
 	public void onMapClick(LatLng point) {
 		// TODO Auto-generated method stub
-		if(posMarker != null){
+		if (posMarker != null) {
 			posMarker.remove();
 		}
-		posMarker = gmap.addMarker(new MarkerOptions()
-			.position(point)
-			.title(getResources().getString(R.string.posMarker))
-			.anchor(0.5f, 0.5f)
-				);
-		
-		
+		posMarker = gmap.addMarker(new MarkerOptions().position(point)
+				.title(getResources().getString(R.string.posMarker))
+				.anchor(0.5f, 0.5f));
+
 	}
-	
-	private LatLng getLocation(){
-		LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE) ;
+
+	private LatLng getLocation() {
+		LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		Criteria criteria = new Criteria();
 		String proveedor = manager.getBestProvider(criteria, true);
 		Location localizacion = manager.getLastKnownLocation(proveedor);
-		return new LatLng(localizacion.getLatitude(), localizacion.getLongitude());
+		return new LatLng(localizacion.getLatitude(),
+				localizacion.getLongitude());
 	}
 
 }
